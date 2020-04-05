cb23e6ac3039c488ff4bb03d1a66316538cf8dca
Олеся <Olesya@MacBook-Air-Olesa-2.local>
2020-04-05 23:54:23 +0300

    Декомпозировала код

--- forJSON.py
+++ forJSON.py
@@ -1,35 +1,34 @@
-# топ 10 самых часто встречающихся в новостях слов длиннее 6 символов для каждого файла.
-
 from pprint import pprint
 from collections import Counter
 
 
+def find_descriptions():
+    import json
+    with open("newsafr.json") as datafile:
+        json_data = json.load(datafile)
+        all_descriptions = json_data["rss"]["channel"]["items"]
+
+        union_description = []
+        for item in all_descriptions:
+            description = item["description"].split()
+            union_description += description
+    return union_description
+
+
+def find_list_of_words(union_description):
+    list_of_words = []
+    for word in union_description:
+        if len(word) > 6:
+            list_of_words.append(word)
+    return list_of_words
+
+
 def popular_words():
-
-    def find_descriptions():
-        import json
-        with open("newsafr.json") as datafile:
-            json_data = json.load(datafile)
-            all_descriptions = json_data["rss"]["channel"]["items"]
-
-            union_description = []
-            for item in all_descriptions:
-                description = item["description"].split()
-                union_description += description
-        return union_description
-
     union_description = find_descriptions()
-
-    def find_list_of_words():
-        list_of_words = []
-        for word in union_description:
-            if len(word) > 6:
-                list_of_words.append(word)
-        return list_of_words
-
-    list_of_words = find_list_of_words()
+    list_of_words = find_list_of_words(union_description)
 
     popular_words = Counter(list_of_words)
     pprint(popular_words.most_common(10))
+
 
 popular_words()
--- forXML.py
+++ forXML.py
@@ -2,35 +2,34 @@ from pprint import pprint
 from collections import Counter
 
 
+def find_descriptions():
+    import xml.etree.ElementTree as ET
+    parser = ET.XMLParser(encoding="utf-8")
+
+    with open("newsafr.xml") as datafile:
+        tree = ET.parse("newsafr.xml", parser)
+        root = tree.getroot()
+        channel = root.find("channel")
+        items = channel.findall("item")
+        union_description = []
+        for item in items:
+            description = item.find("description").text
+            description = description.split()
+            union_description += description
+    return union_description
+
+
+def find_list_of_words(union_description):
+    list_of_words = []
+    for word in union_description:
+        if len(word) > 6:
+            list_of_words.append(word)
+    return list_of_words
+
+
 def popular_words():
-
-    def find_descriptions():
-
-        import xml.etree.ElementTree as ET
-        parser = ET.XMLParser(encoding="utf-8")
-
-        with open("newsafr.xml") as datafile:
-            tree = ET.parse("newsafr.xml", parser)
-            root = tree.getroot()
-            channel = root.find("channel")
-            items = channel.findall("item")
-            union_description = []
-            for item in items:
-                description = item.find("description").text
-                description = description.split()
-                union_description += description
-        return union_description
-
     union_description = find_descriptions()
-
-    def find_list_of_words():
-        list_of_words = []
-        for word in union_description:
-            if len(word) > 6:
-                list_of_words.append(word)
-        return list_of_words
-
-    list_of_words = find_list_of_words()
+    list_of_words = find_list_of_words(union_description)
 
     popular_words = Counter(list_of_words)
     pprint(popular_words.most_common(10))
