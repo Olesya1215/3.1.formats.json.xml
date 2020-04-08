097b6932567f7bdfe7fbbf93fb780948dddaf1d7
Олеся <Olesya@MacBook-Air-Olesa-2.local>
2020-04-08 21:21:32 +0300

    Внесла исправления

--- popular_words_counter.py
+++ popular_words_counter.py
@@ -2,7 +2,20 @@ from pprint import pprint
 from collections import Counter
 
 
-def find_descriptions():
+def parse_json():
+    import json
+    with open("newsafr.json") as datafile:
+        json_data = json.load(datafile)
+        all_descriptions = json_data["rss"]["channel"]["items"]
+
+        union_description = []
+        for item in all_descriptions:
+            description = item["description"].split()
+            union_description += description
+    return union_description
+
+
+def parse_xml():
     import xml.etree.ElementTree as ET
     parser = ET.XMLParser(encoding="utf-8")
 
@@ -23,15 +36,22 @@ def find_list_of_words(union_description):
     list_of_words = []
     for word in union_description:
         if len(word) > 6:
-            list_of_words.append(word)
+            list_of_words.append(word.casefold())
     return list_of_words
 
 
-def popular_words():
-    union_description = find_descriptions()
+def popular_words(format):
+    if format == 'json':
+        union_description = parse_json()
+    elif format == 'xml':
+        union_description = parse_xml()
+    else:
+        print('Неверный формат файла')
     list_of_words = find_list_of_words(union_description)
 
     popular_words = Counter(list_of_words)
     pprint(popular_words.most_common(10))
 
-popular_words()
+
+popular_words('json')
+popular_words('xml')
